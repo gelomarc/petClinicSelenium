4562397a3aff7225adfa0fcd103881633a96e9f4
gelomarc <gelomarc@gmail.com>
2018-02-03 16:18:39 +0100

    add system universality

--- src/main/java/com/petClinic/utils/Utils.java
+++ src/main/java/com/petClinic/utils/Utils.java
@@ -3,17 +3,42 @@ package com.petClinic.utils;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 
 public class Utils {
 
+    public static String getOperatingSystemType() {
+        String detectedOS;
+        String os = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
+        if ((os.indexOf("mac") >= 0) || (os.indexOf("darwin") >= 0)) {
+            detectedOS = "macOS";
+        } else if (os.indexOf("nux") >= 0) {
+            detectedOS = "unix";
+        } else {
+            detectedOS = "windows";
+        }
+        return os;
+    }
+
     public static WebDriver initDriver() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        WebDriver driver = new ChromeDriver(chromeOptions);
+        String actualOperationSystem = getOperatingSystemType();
+        switch (actualOperationSystem) {
+            case "macOS":
+                System.setProperty("webdriver.chrome.driver,", ".\\drivers\\chromedriver_mac");
+                break;
+            case "unix":
+                System.setProperty("webdriver.chrome.driver,", ".\\drivers\\chromedriver_unix");
+                break;
+            case "windows":
+                System.setProperty("webdriver.chrome.driver,", ".\\drivers\\chromedriver_win.exe");
+                break;
+        }
+
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().setSize(new Dimension(2500, 1800));
         return driver;
     }
